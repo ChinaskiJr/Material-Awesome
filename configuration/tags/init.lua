4a2e40baa0185abdb557dc7277e5b69aca13f21c
ChinaskiJr <robin.colombier@protonmail.com>
2021-02-13 22:34:04 +0000

    Change default layouts

--- configuration/tags/init.lua
+++ configuration/tags/init.lua
@@ -56,8 +56,10 @@ local tags = {
 
 awful.layout.layouts = {
   awful.layout.suit.tile,
-  awful.layout.suit.max,
-  awful.layout.suit.floating
+  awful.layout.suit.tile.left,
+  awful.layout.suit.tile.bottom,
+  awful.layout.suit.tile.top,
+  awful.layout.suit.floating,
 }
 
 awful.screen.connect_for_each_screen(
