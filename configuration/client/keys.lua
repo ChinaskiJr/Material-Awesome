53f4679f2a1bdc1a5a4e62f1e9f2b37ef4a7eb70
ChinaskiJr <robin.colombier@protonmail.com>
2021-02-16 22:33:14 +0000

    Add hotkeys for swapping or focus between clients or screens

--- configuration/client/keys.lua
+++ configuration/client/keys.lua
@@ -28,7 +28,35 @@ local clientKeys =
     function (c)
       c:swap(awful.client.getmaster())
     end,
-    {description = "move to master", group = "client"})
+    {description = "move to master", group = "client"}),
+  awful.key(
+    {modkey, "Shift"},
+    "k",
+    function ()
+      awful.client.swap.byidx(  1)
+    end,
+    {description = "swap with next client by index", group = "client"}),
+  awful.key(
+    {modkey, "Shift"},
+    "j",
+    function ()
+      awful.client.swap.byidx( -1)
+    end,
+    {description = "swap with previous client by index", group = "client"}),
+  awful.key(
+    {modkey, "Control"},
+    "k",
+    function ()
+      awful.screen.focus_relative( 1)
+    end,
+    {description = "focus the next screen", group = "screen"}),
+  awful.key(
+    {modkey, "Control"},
+    "j",
+    function ()
+      awful.screen.focus_relative(-1)
+    end,
+    {description = "focus the previous screen", group = "screen"})
 )
 
 return clientKeys
